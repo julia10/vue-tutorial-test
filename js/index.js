4ac565b925a62e26e283b613890f839fc23ddae0
Yuliia Voloshyna <yuliiavoloshyna@MacBook-Pro.local>
2020-06-06 16:29:51 +0100

    computed and filters

--- js/index.js
+++ js/index.js
@@ -1,17 +1,51 @@
 var app = new Vue({
-    el: '#root',
-    data: {
-      message: 'Hello Vue!',
-      title: 'You loaded this element on ' + new Date().toLocaleString(),
-      count: 3
+  el: '#root',
+  data: {
+    message: 'Hello Vue!',
+    title: 'You loaded this element on ' + new Date().toLocaleString(),
+    count: 3
+  }
+})
+
+var app2 = new Vue({
+  el: '#div2',
+  data: {
+    email: ''
+  }
+})
+
+var app3 = new Vue({
+  el: "#div3",
+  data: {
+    cats: [
+      { name: 'marsik' },
+      { name: 'sonya' },
+      { name: 'musya' },
+      { name: 'barsik' }
+    ],
+    newCat: '',
+  },
+
+  methods: {
+    addCat: function () {
+      this.cats.push({ name: this.newCat });
+      this.newCat = '';
     }
-  })
+  },
 
-  var app2 = new Vue({
-      el:'#div2',
-      data: {
-          email:''
+  filters: {
+    capitalize: function (value) {
+      return value.toUpperCase();
+    }
+  },
+
+  computed: {
+    kittifiedName: {
+      get: function () {
+        if (this.newCat.length > 1) {
+          return this.newCat + 'y';
+        }
       }
-  })
-
-  //Finished on 11.35
+    }
+  }
+})
